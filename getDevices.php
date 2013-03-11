22b84a6ac2fcbbcd76bba9bbb8517ae53b14bf2c
Anthony Dagostino <ajd2112@gmail.com>
2013-03-11 11:50:04 -0400

    Added JSON request for incentives, notifications, and device page
    also started parsing devices to html

--- getDevices.php
+++ getDevices.php
@@ -6,31 +6,3 @@ $decode = json_decode($json, true); //getting the file content as array
  echo "$json";
 
 ?>
-        <script>
-            //function called when device page is shown
-            $('#devices').live('pageshow',function(event){
-                alert(2);
-                //code to call webservice
-                $.ajax({
-                  type: 'GET',
-                  url: "http://localhost:8080/com.sparc.webservice/ws/sparc/getDeviceList?callback=getDeviceListCallback",
-                  async: false,
-                  contentType: "application/json",
-                  dataType: 'jsonp'
-                });
-            });
-            //callback function specified in url above this is where you convert teh JSON into html
-            function getDeviceListCallback(response)
-            {
-                //this displays the response in the chrome console, this will be replaced with code to convert
-                // the json into html
-                try // try to output this to the javascript console
-                {
-                    console.log(response);
-                }
-                catch(an_exception) // alert for the users that don't have a javascript console
-                {
-                    alert(an_exception);
-                }
-            }
-      </script> 
